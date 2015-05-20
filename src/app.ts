b670d4c8865c158848d40221330b7b895424571d
Oliver Weichhold <oliver@weichhold.com>
2015-05-20 14:48:28 +0200

    Example registration

--- src/app.ts
+++ src/app.ts
@@ -29,9 +29,15 @@ wx.app.component('welcome', {
     template: <wx.IComponentTemplateDescriptor> <any> { require: "text!components/welcome/index.html" }
 });
 
-wx.app.component('hello', {
-    template: <wx.IComponentTemplateDescriptor> <any> { require: "text!components/hello/index.html" }
-});
+interface IExample {
+    title: string;
+    folder: string;
+    hasViewModel: boolean;
+}
+
+var examples:Array<IExample> = [
+    { title: "Hello World", folder: "hello", hasViewModel: false },
+];
     
 wx.router.state({
     name: "$",
@@ -60,6 +66,48 @@ wx.router.state({
     }
 });
 
+// Register example components & states
+examples.forEach(x=> {
+    if(x.hasViewModel) {
+        wx.app.component(x.folder, {
+            viewModel: <wx.IComponentViewModelDescriptor> <any> { require: wx.formatString("js/components/{0}/ViewModel", x.folder) },
+            template: <wx.IComponentTemplateDescriptor> <any> { require: wx.formatString("text!/components/{0}/index.html", x.folder) }
+        });
+    } else {
+        wx.app.component(x.folder, {
+            template: <wx.IComponentTemplateDescriptor> <any> { require: "text!components/hello/index.html" }
+        });
+    }
+
+    wx.router.state({
+        name: x.folder,
+        views: {
+            'main': {
+                component: x.folder,
+                animations: {
+                    enter: "push-bottom-from-top-enter",
+                    leave: "push-bottom-from-top-leave"
+                }
+            }
+        }
+    });  
+});
+
+this.currentExampleIndex = wx.property(0);
+this.currentExample = wx.whenAny(this.currentExampleIndex, cei=> examples[cei]).toProperty();
+
+this.nextExampleCmd = wx.command(param=> {
+    var index = this.currentExampleIndex();
+    
+    wx.router.go(examples[index].folder, {}, { location: wx.RouterLocationChangeMode.add });
+    
+    if(index + 1 < examples.length - 1)
+        index++;
+    else
+        index = 0;
+        
+    this.currentExampleIndex(index);
+}, this);
 
 var syncUrl = wx.getSearchParameters()["rs"];
 
@@ -69,4 +117,4 @@ if(!syncUrl) {
     wx.router.sync((<wx.IRoute> wx.router.get("$").url).stringify() + syncUrl);
 }
 
-wx.applyBindings(undefined);
+wx.applyBindings(this);
