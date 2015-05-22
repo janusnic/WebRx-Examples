5b16a1f556dddc53ceff969ae1739ce03b7f015a
Oliver Weichhold <oliver@weichhold.com>
2015-05-22 09:50:22 +0200

    Display views in state-monitor

--- src/components/state-monitor/ViewModel.ts
+++ src/components/state-monitor/ViewModel.ts
@@ -7,12 +7,26 @@ class ViewModel {
             self.name(x.name);
             self.route(typeof x.url === "string"? x.url : (<wx.IRoute> <any> x.url).stringify(x.params));
             self.params(x.params ? JSON.stringify(x.params) : "");
+            
+            var views = {};
+            if(x.views) {
+                Object.keys(x.views).forEach(key=> {
+                    var view = x.views[key];
+                    if(typeof view === "string") {
+                        views[key] = view;
+                    } else {
+                        views[key] = view.component;
+                    }
+                });
+            }
+            self.views(JSON.stringify(views));
         });
 	}
 
     public name = wx.property();
     public route = wx.property();
     public params = wx.property();
+    public views = wx.property();
 }
 
 export = ViewModel;
