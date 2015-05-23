ce523069d3851ae241408ad83cbc7b37c3d02540
Oliver Weichhold <oliver@weichhold.com>
2015-05-23 16:20:29 +0200

    Added documentation links

--- src/components/hello-stateful/example.ts
+++ src/components/hello-stateful/example.ts
@@ -1,7 +1,7 @@
 /// <reference path="../../typings/web.rx.d.ts" />
 
 class ViewModel {    
-    public firstName = wx.property(wx.router.current().params.firstName || 'Bart');
+    public firstName = wx.property(wx.router.current().params.firstName || 'Homer');
     public lastName = wx.property(wx.router.current().params.lastName || 'Simpson');
     
     public fullName = wx.whenAny(this.firstName, this.lastName, (firstName, lastName)=> {
