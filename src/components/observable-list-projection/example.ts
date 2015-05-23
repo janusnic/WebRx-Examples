accf0ddf98c4f095c258b9ff067827837b6a5ef1
Oliver Weichhold <oliver@weichhold.com>
2015-05-23 13:22:50 +0200

    Fixed list projections

--- src/components/observable-list-projection/example.ts
+++ src/components/observable-list-projection/example.ts
@@ -13,14 +13,14 @@ class ViewModel {
     ]);
                 
     public typeToShow = wx.property("all");
-    public filter = wx.property();
+    public filter = wx.property<string>();
     public displayAdvancedOptions = wx.property(false);
 
-    public addPlanetCmd = wx.command(function (type) {
+    public addPlanetCmd = wx.command((type)=> {
         this.planets.push({ name: "New planet", type: type });
     }, this);
 
-    public planetsToShow = this.planets.project(function (planet) {
+    public planetsToShow = this.planets.project(function(planet) {
         var desiredType = this.typeToShow();
         var result = desiredType === "all" || planet.type === desiredType;
         
